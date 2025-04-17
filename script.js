6cdf337c6cb9b10a9766fc8d59e77b52227d9805
Caiorossi00 <caio_rossi56@hotmail.com>
2025-04-17 10:06:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.remove("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - 70,
+      behavior: "smooth",
+    });
+  });
+});
